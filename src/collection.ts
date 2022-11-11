1b37917269a347094901790f3fbdf50736e262c6
caramboleyo <caramboleyo>
2022-11-11 23:41:26 +0100

    introduced collection as a replacement for dataset in mod.ts

---
+++ src/collection.ts
@@ -0,0 +1,144 @@
+import type { DataObject, DbResults, Mongobj, Projection } from './types.ts';
+import {
+	_find,
+	_findOne,
+	_insert,
+	_remove,
+	_removeOne,
+	_update,
+	_updateOne,
+} from './methods/mod.ts';
+import { init } from './storage.ts';
+import type DataStoreOptions from './types/ds.options.ts';
+import { EventEmitter, resolve } from '../deps.ts';
+import Executor from './executor.ts';
+
+/** Represents the Datastore instance. */
+export default class Collection<Doc extends DataObject> extends EventEmitter {
+	public filename: string;
+	private bufSize?: number;
+	private executor = new Executor();
+
+	/** Builds the datastore with the given options. */
+	constructor({ filename, autoload, bufSize }: DataStoreOptions) {
+		super();
+		this.filename = filename
+			? resolve(Deno.cwd(), filename)
+			: resolve(Deno.cwd(), './database.json');
+		this.bufSize = bufSize;
+		if (autoload) {
+			this.loadDatabase().then(() => this.emit('load'));
+		}
+	}
+
+	/** Loads the database on first load and ensures that path exists. */
+	loadDatabase() {
+		return init(this.filename);
+	}
+
+	/** Finds multiple matching documents. */
+	async find(
+		query: Partial<Doc>,
+		projection: Partial<Projection<keyof Doc>> = {},
+		cb?: (x: DbResults<Doc>) => void
+	) {
+		const results = (await this.executor.add(_find, [
+			this.filename,
+			query,
+			projection,
+			this.bufSize,
+		] as const)) as DbResults<Doc>;
+		if (cb && typeof cb === 'function') {
+			cb(results);
+		}
+		return results;
+	}
+
+	/** Find first matching document. */
+	async findOne(
+		query: Partial<Doc>,
+		projection: Partial<Projection<keyof Doc>> = {},
+		cb?: (x: DbResults<Doc>) => void
+	) {
+		const results = (await this.executor.add(_findOne, [
+			this.filename,
+			query,
+			projection,
+			this.bufSize,
+		] as const)) as DbResults<Doc>;
+		if (cb && typeof cb === 'function') {
+			cb(results);
+		}
+		return results;
+	}
+
+	/** Insert a document. */
+	async insert(data: Doc, cb?: (x: DbResults<Doc>) => void) {
+		const results = (await this.executor.add(_insert, [
+			this.filename,
+			data,
+		] as const)) as DbResults<Doc>;
+		if (cb && typeof cb === 'function') {
+			cb(results);
+		}
+		return results;
+	}
+
+	/** Update multiple matching documents */
+	async update(query: Partial<Doc>, operators: Mongobj<Doc>, cb?: (x: DbResults<Doc>) => void) {
+		const results = (await this.executor.add(_update, [
+			this.filename,
+			query,
+			operators,
+			this.bufSize,
+		] as const)) as DbResults<Doc>;
+		if (cb && typeof cb === 'function') {
+			cb(results);
+		}
+		return results;
+	}
+
+	/** Update first matching document */
+	async updateOne(
+		query: Partial<Doc>,
+		operators: Mongobj<Doc>,
+		cb?: (x: DbResults<Doc>) => void
+	) {
+		const results = (await this.executor.add(_updateOne, [
+			this.filename,
+			query,
+			operators,
+			this.bufSize,
+		] as const)) as DbResults<Doc>;
+		if (cb && typeof cb == 'function') {
+			cb(results);
+		}
+		return results;
+	}
+
+	/** Remove multiple matching documents */
+	async remove(query: Partial<Doc>, cb?: (x: DbResults<Doc>) => void) {
+		const results = (await this.executor.add(_remove, [
+			this.filename,
+			query,
+			this.bufSize,
+		] as const)) as DbResults<Doc>;
+		if (cb && typeof cb === 'function') {
+			cb(results);
+		}
+		return results;
+	}
+
+	/** Remove first matching document */
+	async removeOne(query: Partial<Doc>, cb?: (x: DbResults<Doc>) => void) {
+		const results = (await this.executor.add(_removeOne, [
+			this.filename,
+			query,
+			this.bufSize,
+		] as const)) as DbResults<Doc>;
+		if (cb && typeof cb === 'function') {
+			cb(results);
+		}
+		return results;
+	}
+}
--- src/storage.ts
+++ src/storage.ts
@@ -1,83 +1,78 @@
-import { EventEmitter, existsSync, readLines, writeAllSync } from "../deps.ts";
+import { EventEmitter, existsSync, readLines, writeAllSync } from '../deps.ts';
 
 type Doc = Record<string, unknown>;
 
 /** Ensure datastore initialization on first load */
 export async function init(filename: string) {
-  if (!existsSync(filename)) {
-    await Deno.writeFile(filename, new TextEncoder().encode(""));
-  }
+	if (!existsSync(filename)) {
+		await Deno.writeFile(filename, new TextEncoder().encode(''));
+	}
 }
 
 /** Write file line by line on a stream */
 export class WriteFileStream extends EventEmitter {
-  private file: Deno.File;
-  private updatedFile: string;
-  constructor(private filename: string) {
-    super();
-    this.updatedFile = `${this.filename}.updated`;
-    if (existsSync(this.updatedFile)) {
-      Deno.renameSync(this.updatedFile, this.filename);
-    }
-    this.file = Deno.openSync(this.updatedFile, { write: true, create: true });
-  }
-  public write(data: Doc) {
-    writeAllSync(
-      this.file,
-      new TextEncoder().encode(JSON.stringify(data) + "\n"),
-    );
-    return Promise.resolve(true);
-  }
-  public async end() {
-    Deno.close(this.file.rid);
-    await Deno.rename(this.updatedFile, this.filename);
-    this.emit("close");
-  }
+	private file: Deno.FsFile;
+	private updatedFile: string;
+	constructor(private filename: string) {
+		super();
+		this.updatedFile = `${this.filename}.updated`;
+		if (existsSync(this.updatedFile)) {
+			Deno.renameSync(this.updatedFile, this.filename);
+		}
+		this.file = Deno.openSync(this.updatedFile, { write: true, create: true });
+	}
+	public write(data: Doc) {
+		writeAllSync(this.file, new TextEncoder().encode(JSON.stringify(data) + '\n'));
+		return Promise.resolve(true);
+	}
+	public async end() {
+		Deno.close(this.file.rid);
+		await Deno.rename(this.updatedFile, this.filename);
+		this.emit('close');
+	}
 }
 
 /** Append file line by line */
 export async function writeFile(filename: string, data: Doc) {
-  await ensureExists(filename);
-  await Deno.writeFile(
-    filename,
-    new TextEncoder().encode(JSON.stringify(data) + "\n"),
-    { append: true },
-  );
+	await ensureExists(filename);
+	await Deno.writeFile(filename, new TextEncoder().encode(JSON.stringify(data) + '\n'), {
+		append: true,
+	});
 }
 
 /** Reads the datastore by streaming and buffering chunks */
 export class ReadFileStream extends EventEmitter {
-  constructor(private filename: string, private bufSize?: number) {
-    super();
-    this.stream();
-  }
-  async stream() {
-    const file = await Deno.open(this.filename);
-    for await (const line of readLines(file)) {
-      if (line.length === 0) {
-        continue;
-      }
-      this.emit("document", JSON.parse(line));
-    }
-    file.close();
-    this.emit("end");
-  }
+	constructor(private filename: string, private bufSize?: number) {
+		super();
+		this.stream();
+	}
+	async stream() {
+		const file = await Deno.open(this.filename);
+		for await (const line of readLines(file)) {
+			if (line.length === 0) {
+				continue;
+			}
+			this.emit('document', JSON.parse(line));
+		}
+		file.close();
+		this.emit('end');
+	}
 }
 
 /** Ensures data if file doesn't exists */
 async function ensureExists(filename: string) {
-  if (!existsSync(filename)) await check(() => existsSync(filename), 100);
-  return;
+	if (!existsSync(filename)) await check(() => existsSync(filename), 100);
+	return;
 }
 
 /** Check Polyfill */
 function check(condition: () => boolean, checkTime: number) {
-  return new Promise((resolve) => {
-    const timerId = setInterval(() => {
-      if (condition()) {
-        clearInterval(timerId);
-        resolve("done");
-      }
-    }, checkTime);
-  });
+	return new Promise(resolve => {
+		const timerId = setInterval(() => {
+			if (condition()) {
+				clearInterval(timerId);
+				resolve('done');
+			}
+		}, checkTime);
+	});
 }
